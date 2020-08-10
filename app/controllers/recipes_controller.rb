db2d52effaccca3d76690aa9500f7f9df1c21d0e
Toni <tonitamburkovski@gmail.com>
2020-08-10 16:37:43 +0200

    created controller and form for New Recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,25 @@
 class RecipesController < ApplicationController
   def index
+    @recipes = Recipe.all
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    recipe = Recipe.new(recipe_params)
+    recipe.save
+
+    redirect_to recipe_path(recipe)
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).premit(:title, :description)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  root 'recipes#index'
   get 'recipes/index'
   get '/recipes', to: 'recipes#index'
+  get '/recipes/new', to: 'recipes#new', as: :new_recipe
+  get '/recipes/:id', to: 'recipes#show', as: :recipe
+  post '/recipes', to: 'recipes#create'
 
-  root "recipes#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
