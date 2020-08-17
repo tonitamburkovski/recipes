71e365a35b9f9821c38595719ee7b7c638957f62
Toni <tonitamburkovski@gmail.com>
2020-08-17 10:41:36 +0200

    solving typo for user-recipe association and added user profile button in nav_bar

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,10 +9,8 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       log_in(user)
       redirect_to user
-      # Log the user in and redirect to the user's show page.
     else
-      # Create an error message.
-      flash.now[:danger] = 'Invalid email/password combination' # Not quite right!
+      flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
   end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,8 +3,14 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  def current_user
+    if session[:user_id]
+      @current_user ||= User.find_by(id: session[:user_id])
+    end
+  end
+
   def logged_in?
-    @current_user.present?
+    current_user.present?
   end
 
   def log_out
